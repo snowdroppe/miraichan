de2715d5870091ba5458f0b5f241c7f990b7efdd
snowdroppe <stefanrieche@gmail.com>
2024-02-10 15:24:15 +0000

    Implemented workers and threading

--- miraichan.py
+++ miraichan.py
@@ -7,12 +7,14 @@ import argparse
 import requests
 from itertools import product
 
-proxy = None    # Passed as is to requests context
-user_string = ' '   # User agent string
-cookie = None   # Cookie to use during requests
-retries = 3   # Number of retries to attempt before timing out
-page_size = 10  # How many targets a worker should grab 
+buffer_size = 10  # Input buffer size of worker (bytes)
 debug = True    # Be very verbose about packet requests / responses
+proxies = None  # Passed to requests as-is
+headers = {
+        "Cookie": None,
+        "User-Agent": "python/2.7"
+        }
+
 
 """
 timeout /host /port
@@ -20,8 +22,9 @@ timeout /host /port
 rejection /host /port
     -packet rejected
     -non http response
-fail /host /port /path /cred
+noauth
     -fail precheck i.e. no WWW-Authenticate header
+fail /host /port /path /cred
     -bad creds i.e. 401 with creds
     -contains fail string
 success
@@ -29,47 +32,92 @@ success
     -break on success
 """
 
-class TimeoutError(Exception):
+class _TimeoutError(Exception):
     pass
 
-class RejectionError(Exception):
+class _RejectionError(Exception):
     pass
 
-class FailError(Exception):
+class _FailError(Exception):
     pass
+
+class _NoAuthError(Exception):
+    pass
+
+class FileMutex:
+    def __init__(self, file, mutex):
+        self.file = file
+        self.mutex = mutex
 
 class SmartGet:
     def __init__(self, args):
+        self.timeout = args.timeout
         self.verify = not args.insecure
         self.skip = args.no_precheck
-        self.headers = headers
         self.success = args.success
         self.fail = args.fail
+        self.headers = headers
+        self.proxies = proxies
     
-    def brute(self, url, creds):
+    def run(self, url, creds):
         try:
             if (not self.skip):
                 # Perform precheck for www-authenticate header
-                r = requests.get(url=url, auth=None,
-                                 headers=self.headers, verify=self.insecure)
+                if (debug):
+                    print("="*10 + url + "="*10)
+                r = requests.get(url=url, auth=None, timeout=self.timeout,
+                                 proxies=self.proxies, headers=self.headers,
+                                 verify=self.verify)
+                if(debug):
+                    print("python --> server")
+                    print(r.request.headers)
+                    print("python <-- server")
+                    print(r.headers)
                 if ('www-authenticate' not in r.headers.keys()):
-                    raise FailError
-        except Exception:   # TODO handle exceptions
-            pass
+                    raise _NoAuthError
+        except requests.exceptions.ConnectionError as e:
+            if ("Connection refused" in str(e)):
+                raise _RejectionError
+            elif ("Failed to resolve" in str(e)):
+                raise _TimeoutError
+        except (requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
+            raise _TimeoutError
+        except requests.exceptions.SSLError:
+            raise _FailError  # HTTPS request on HTTP listener
+        except Exception as e:
+            if (debug):
+                raise e
+            raise _FailError    # Hail mary
 
         for c in creds:
+            c = tuple(c)
             try:
-                r = requests.get(url=url, auth=c,
-                                 headers=self.headers, verify=self.insecure)
+                r = requests.get(url=url, auth=c, timeout=self.timeout,
+                                 proxies=self.proxies, headers=self.headers,
+                                 verify=self.verify)
+                if (debug):
+                    print("python --> server")
+                    print(r.request.headers)
+                    print("python <-- server")
+                    print(r.headers)
                 # There is an edge case where redirection to a different domain
                 # causes the Authorization header to be dropped. This is by design
                 # so we work around this by retrying when redirected.
                 # See https://github.com/psf/requests/issues/2949 and CVE-2014-1829
                 if (r.history != []):
                     # Update url to follow final redirection
-                    url = r.requests.url
-                    r = requests.get(url=url, auth=c,
-                                     headers=self.headers, verify=self.insecure)
+                    if (debug):
+                        print("---- REDIRECTING [%d] ----" % (len(r.history),))
+                    url = r.request.url
+                    r = requests.get(url=url, auth=c, timeout=self.timeout,
+                                     proxies=self.proxies, headers=self.headers,
+                                     verify=self.verify)
+                    if(debug):
+                        print("python --> server")
+                        print(r.request.headers)
+                        print("python <-- server")
+                        print(r.headers)
+
 
                 if (r.status_code == 401):
                     continue
@@ -80,49 +128,131 @@ class SmartGet:
                 else:
                     return c    # Success!
 
+            # TODO spend more time exhausting possible exceptions
+            except requests.exceptions.ConnectionError as e:
+                if ("Connection refused" in str(e)):
+                    raise _RejectionError
+                elif ("Failed to resolve" in str(e)):
+                    raise _TimeoutError
+            except (requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
+                raise _TimeoutError
+            except requests.exceptions.SSLError:
+                raise _FailError  # HTTPS request on HTTP listener
+            except Exception as e:
+                if (debug):
+                    raise e
+                raise _FailError    # Hail mary
 
-            except Exception: # TODO handle excpetions
-                pass
-        raise FailError  # Exhausted all creds
+        raise _FailError  # Exhausted all creds
 
 def sigint_handler(signal):
     pass
 
 
-def worker(mutex_in, fd_in, mutex_out, fd_out):
+def worker(t_fm, o_fm, schemes, paths, creds):
+    def pull(t_fm):
+        with t_fm.mutex:
+            targets = t_fm.file.readlines(buffer_size) 
+        if (targets == []):
+            return None
+        # Clean up any whitespace and naughty characters (dirty but quick)
+        targets = [(i.replace("\n", "").replace(" ", "").replace("\t", "")
+                    .replace(":", "").replace(",", "")) for i in targets]
+        return targets
+
+    def push(o_fm, buf):
+        with o_fm.mutex:
+            o_fm.file.write("\n".join(buf)+"\n")
+
     smart = SmartGet(args)
-    #for t in targets:
-    #    for p in ports:
+
+    while(True):
+        targets = pull(t_fm)
+        # Aggregate logs together so we can write once
+        buf = []
+        if (targets == None):
+            break
+        for t in targets:
+            for s in schemes:
+                for p in paths:
+                    url = "%s://%s:%d%s" % (s[0], t, s[1], p)
+                    try:
+                        r = smart.run(url, creds)
+                        buf.append("success,%s,%s,%d,%s" % (s[0], t, s[1], p))
+                    except _FailError:
+                        buf.append("fail,%s,%s,%d,%s" % (s[0], t, s[1], p))
+                    except _NoAuthError:
+                        buf.append("noauth,%s,%s,%d,%s" % (s[0], t, s[1], p))
+                    except _RejectionError:
+                        buf.append("rejected,%s,%s,%d,%s" % (s[0], t, s[1], ""))
+                        break   # Give up and try next scheme
+                    except _TimeoutError:
+                        buf.append("timeout,%s,%s,%d,%s" % (s[0], t, s[1], ""))
+                        break   # Give up and try next scheme
+        push(o_fm, buf)
+
 
 def main(args):
+    # TODO catch and handle sigint
+    print("")
     # Calculate protocol:port pairs
     if (not args.ports):
         if (args.tls):
-            schemes = [["https://",443]]
+            schemes = [["https",443]]
         elif (args.no_tls):
-            schemes = [["http://",80]]
+            schemes = [["http",80]]
         else:
-            schemes = [["http://",80], ["https://",443]]
+            schemes = [["http",80], ["https",443]]
     else:
         if (args.tls):
-            proto = ["https://"]
+            proto = ["https"]
         elif (args.no_tls):
-            proto = ["https://"]
+            proto = ["https"]
         else:
-            proto = ["http://", "https://"]
+            proto = ["http", "https"]
         schemes = product(proto, ports)
-    # Calculate credential pairs
-    if (args.clusterbomb):
-        creds = product(usernames, passwords)
-    else:
-        if (len(usernames) != len(passwords)):
-            raise IndexError("Dissimilar sized username and password"
-                             "lists cannot be used with pitchfork")
-        creds = [list(i) for i in zip(usernames, passwords)]
-    # TODO catch and handle sigint
-    # TODO open file handles
-    # TODO spawn workers
 
+    # Calculate credential pairs
+    usernames, passwords = None, None
+    if (args.logins):
+        with open(args.logins, "r") as f:
+            usernames = f.readlines()
+            usernames = [i.replace("\n", "") for i in usernames]
+    if (args.passwords):
+        with open(args.passwords, "r") as f:
+            passwords = f.readlines()
+            passwords = [i.replace("\n", "") for i in passwords]
+
+    if (usernames and passwords):
+        if (args.clusterbomb):
+            creds = product(usernames, passwords)
+        else:
+            if (len(usernames) != len(passwords)):
+                raise IndexError("Dissimilar sized username and password"
+                                 "lists cannot be used with pitchfork")
+            creds = [list(i) for i in zip(usernames, passwords)]
+    elif (usernames):
+        creds = product(usernames, [args.password])
+    elif (passwords):
+        creds = product([args.login], passwords)
+    else:
+        creds = [[args.login, args.password]]
+
+    t_fm = FileMutex(open(args.targets, "r"), threading.Lock())
+    o_fm = FileMutex(open(args.outfile, "w"), threading.Lock())
+    threads = []
+    for i in range(args.threads):
+        t = threading.Thread(target=worker, args=(t_fm, o_fm, schemes, args.paths, creds))
+        threads.append(t)
+    if (debug):
+        print(t)
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+    t_fm.file.close()
+    o_fm.file_close()
+    print("Woooooooo coffee break :D")
 
 
 if (__name__ == "__main__"):
@@ -181,4 +311,4 @@ if (__name__ == "__main__"):
     group7.add_argument("--end", type=int, help="end line (inclusive)")
 
     args = parser.parse_args()
-    print(args)
+    main(args)
